c299b8bd73d01470011d761465d55b3106090764
Eduardo Rodrigues <56962642+EduardoAFR@users.noreply.github.com>
2024-11-25 21:11:54 -0300

    Update autor.py

--- models/autor.py
+++ models/autor.py
@@ -1,11 +1,9 @@
-# models/autor.py
 from connection.connection import conectar
 
 class Autor:
-    def __init__(self, nome, nacionalidade, data_nascimento):
+    def __init__(self,id_autor, nome):
+        self.id_autor = id_autor
         self.nome = nome
-        self.nacionalidade = nacionalidade
-        self.data_nascimento = data_nascimento
 
     @staticmethod
     def listar_autores():
@@ -14,24 +12,24 @@ class Autor:
         return list(collection.find())
 
     @staticmethod
-    def criar_autor(nome, nacionalidade, data_nascimento):
+    def criar_autor(id_autor,nome):
         db = conectar()
         collection = db["autores"]
         novo_autor = {
-            "nome": nome,
-            "nacionalidade": nacionalidade,
-            "data_nascimento": data_nascimento
-        }
+            "IdAutor": int(id_autor),
+            "nome": nome
+            }
         result = collection.insert_one(novo_autor)
-        return result.inserted_id
+        return {"sucesso": True, "mensagem": f"Autor criado com sucesso!"}
 
     @staticmethod
-    def atualizar_autor(id_autor, novos_dados):
+    def atualizar_autor(id_autor, nome):
         db = conectar()
         collection = db["autores"]
+
         result = collection.update_one(
-            {"_id": id_autor},
-            {"$set": novos_dados}
+            {"IdAutor": int(id_autor)},
+            {"$set": {"nome": nome}}
         )
         return result.modified_count
 
@@ -39,5 +37,13 @@ class Autor:
     def deletar_autor(id_autor):
         db = conectar()
         collection = db["autores"]
+
+        # Verifica se o autor está sendo referenciado em livros
+        livros_collection = db["livros"]
+        livro_count = livros_collection.count_documents({"autor": id_autor})
+
+        if livro_count > 0:
+            return False  # Não pode excluir o autor, pois está em um livro
+
         result = collection.delete_one({"_id": id_autor})
         return result.deleted_count
