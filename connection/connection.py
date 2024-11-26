1a881901286a18b50d0fb737d0b9af085e591ce4
Eduardo Rodrigues <56962642+EduardoAFR@users.noreply.github.com>
2024-11-25 21:15:33 -0300

    Update connection.py

--- connection/connection.py
+++ connection/connection.py
@@ -1,7 +1,38 @@
 from pymongo import MongoClient
+import logging
+
+# Configuração de logging
+logger = logging.getLogger(name="Library_DB_Initialization")
+logger.setLevel(level=logging.WARNING)
+
+# Lista de coleções necessárias no banco de dados
+LIST_OF_COLLECTIONS = ["autores", "livros", "pedidos", "generos"]
 
 def conectar():
-    # Replace the URL with your MongoDB instance details
+    """
+    Estabelece uma conexão com o banco de dados MongoDB e retorna a instância do banco.
+    """
     client = MongoClient("mongodb://localhost:27017/")
-    db = client["LibraryDB"]  # Specify your database name
-    return db
+    return client["libraryDB"]
+
+def initialize_database(drop_if_exists: bool = False):
+    """
+    Inicializa o banco de dados MongoDB criando as coleções necessárias.
+
+    Parâmetros:
+    - drop_if_exists: Se True, exclui as coleções existentes e as recria.
+    """
+    db = conectar()
+    existing_collections = db.list_collection_names()  # Lista as coleções existentes no banco
+    
+    for collection in LIST_OF_COLLECTIONS:
+        if collection in existing_collections:
+            if drop_if_exists:
+                db.drop_collection(collection)  # Remove a coleção existente
+                logger.warning(f"{collection} dropped!")
+                db.create_collection(collection)  # Recria a coleção
+                logger.warning(f"{collection} created!")
+        else:
+            db.create_collection(collection)  # Cria a coleção caso não exista
+            logger.warning(f"{collection} created!")
+    logger.warning("Database initialization complete.")
